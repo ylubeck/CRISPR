8e1191e52455a490b0eb39f3e170616c11649fa6
ylubeck <y.s.lubeck@gmail.com>
2019-02-05 16:56:46 +0100

    Add files via upload

---
+++ guiargs.py
@@ -0,0 +1,20 @@
+#class for args CrisprCasFinderGui
+
+class GUIargs:
+    def __init__(self,input,output,reverse,viralfastas,blastviraldb,minimum,maximum,percidentity,statistics,evidencethreshold):
+        self.input = input
+        self.output = output
+        self.reverse = reverse
+        self.viralfastas = viralfastas
+        self.blastviraldb = blastviraldb
+        self.minimum = minimum
+        self.maximum = maximum
+        self.percidentity = percidentity
+        self.statistics = statistics
+        self.evidencethreshold = evidencethreshold
+
+    def printVars(self):
+        print("input: " + self.input)
+        print("output: " + self.output)
+        print("reverse: "  + str(self.reverse))
+        print("stats: " + str(self.statistics))
